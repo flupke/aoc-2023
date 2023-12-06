0d1d66a8cf69fe9136af5a060457df7804da6c5f
Luper Rouch <luper.rouch@gmail.com>
2023-12-06 22:08:21 +0100

    day 2: implemented part two

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -2,34 +2,57 @@ use aoc_2023_rust_flupke::Problem;
 
 pub struct Day2;
 
-fn is_possible_game(line: &str, max_red: u8, max_green: u8, max_blue: u8) -> (u16, bool) {
-    let (prefix, suffix) = line.split_once(":").unwrap();
-    let game_id = prefix.split_once(" ").unwrap().1.parse::<u16>().unwrap();
-    for draft in suffix.split(";") {
-        for draft_entry in draft.split(",") {
-            let (number, color) = draft_entry.trim().split_once(" ").unwrap();
-            let number = number.parse::<u8>().unwrap();
-            match color {
-                "red" => {
-                    if number > max_red {
-                        return (game_id, false);
-                    }
-                }
-                "green" => {
-                    if number > max_green {
-                        return (game_id, false);
-                    }
-                }
-                "blue" => {
-                    if number > max_blue {
-                        return (game_id, false);
-                    }
-                }
-                _ => panic!("Unknown color {}", color),
-            }
+struct Draft {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Draft {
+    fn new() -> Draft {
+        Draft {
+            red: 0,
+            green: 0,
+            blue: 0,
         }
     }
-    return (game_id, true);
+
+    fn power(&self) -> u32 {
+        self.red * self.green * self.blue
+    }
+}
+
+fn parse_draft(draft: &str) -> Draft {
+    let mut result = Draft::new();
+    for draft_entry in draft.split(",") {
+        let (number, color) = draft_entry.trim().split_once(" ").unwrap();
+        let number = number.parse::<u32>().unwrap();
+        match color {
+            "red" => result.red += number,
+            "green" => result.green += number,
+            "blue" => result.blue += number,
+            _ => panic!("Unknown color {}", color),
+        }
+    }
+    result
+}
+
+fn get_game_power(line: &str) -> u32 {
+    let suffix = line.split_once(":").unwrap().1;
+    let mut min_draft = Draft::new();
+    for draft in suffix.split(";") {
+        let draft = parse_draft(draft);
+        if draft.red > min_draft.red {
+            min_draft.red = draft.red;
+        }
+        if draft.green > min_draft.green {
+            min_draft.green = draft.green;
+        }
+        if draft.blue > min_draft.blue {
+            min_draft.blue = draft.blue;
+        }
+    }
+    return min_draft.power();
 }
 
 impl Problem for Day2 {
@@ -38,16 +61,12 @@ impl Problem for Day2 {
     }
 
     fn solve(&self) {
-        let mut possible_games_sum = 0;
-        std::fs::read_to_string("src/day_2/input.txt")
+        let powers_sum = std::fs::read_to_string("src/day_2/input.txt")
             .unwrap()
             .lines()
-            .for_each(|line| {
-                let (game_id, is_possible) = is_possible_game(line, 12, 13, 14);
-                if is_possible {
-                    possible_games_sum += game_id;
-                }
-            });
-        println!("Possible games sum: {}", possible_games_sum);
+            .map(|line| get_game_power(line))
+            .reduce(|a, b| a + b)
+            .unwrap();
+        println!("Possible games sum: {}", powers_sum);
     }
 }
